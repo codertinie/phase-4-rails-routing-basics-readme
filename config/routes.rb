50aa8bfd1a1170f7fe01ff31ea58745954b0f562
codertinie <martin.munyao@moringaschool.com>
2022-11-22 15:41:27 +0300

    Completed assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'cheentroller/Cheeses'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "cheeses", to: "cheeses#index"
 end
